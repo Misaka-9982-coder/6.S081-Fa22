ab559133789617e90f7b536963b431c452e67b6a
Misaka <chenborun1101@gmail.com>
2023-08-23 23:27:19 +0800

    copy-on-write

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -39,6 +39,10 @@ freerange(void *pa_start, void *pa_end)
     kfree(p);
 }
 
+// the reference count of physical memory page
+int useReference[PHYSTOP / PGSIZE];
+struct spinlock ref_count_lock;
+
 // Free the page of physical memory pointed at by pa,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
@@ -47,9 +51,18 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int temp;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  acquire(&ref_count_lock);
+  // decrease the reference count, if use reference is not zero, then return
+  useReference[(uint64)pa/PGSIZE] -= 1;
+  temp = useReference[(uint64)pa/PGSIZE];
+  release(&ref_count_lock);
+  if (temp > 0)
+    return;
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
@@ -72,6 +85,15 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
+
+  if(r) {
+    kmem.freelist = r->next;
+    acquire(&ref_count_lock);
+    // initialization the ref count to 1
+    useReference[(uint64)r / PGSIZE] = 1;
+    release(&ref_count_lock);
+  }
+
   if(r)
     kmem.freelist = r->next;
   release(&kmem.lock);
